4f867104634d0c48bb32da2ef62ec43e55621ebc
Varsha <hp@DESKTOP-4IENAI5>
2020-05-08 16:52:16 +0530

    first commit

---
+++ linking.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  var text = document.getElementById("myText").value;
+  document.getElementById("print").innerHTML =
+    "Thank you " + "'" + text + "'" + "  for entering your name ";
+}
