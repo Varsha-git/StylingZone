07ee56a3562ccd73806d494b470e7f82867e103b
Varsha <hp@DESKTOP-4IENAI5>
2020-05-08 11:55:48 +0530

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var arr = { h: 0, d: 0, j: 0 };
+var arr = { h: 0, d: 0, j: 0, bg: 0, sc: 0, sg: 0 };
 console.log(arr.h);
 console.log(arr.d);
 console.log(arr.j);
+console.log(arr.bg);
+console.log(arr.sc);
+console.log(arr.sg);
+
+//var name = prompt("Please enter your name ...");
+//document.write("Hello " + name + "  Lets play... ");
+
 //let hair_click = document.getElementById("hairstyle");
 //hair_click.onclick = nextHair;
 //console.log(hair_click);
 
+//functionNextHair
 function nextHair() {
   console.log("inside function nexthair");
   var hair = document.getElementById("hairstyle");
@@ -32,6 +40,34 @@ function nextHair() {
     console.log(arr.h);
   } else if (arr.h === 5) {
     hair.setAttribute("class", "hairstyle6");
+    arr.h++;
+    console.log(arr.h);
+  } else if (arr.h === 6) {
+    hair.setAttribute("class", "hairstyle7");
+    arr.h++;
+    console.log(arr.h);
+  } else if (arr.h === 7) {
+    hair.setAttribute("class", "hairstyle8");
+    arr.h++;
+    console.log(arr.h);
+  } else if (arr.h === 8) {
+    hair.setAttribute("class", "hairstyle9");
+    arr.h++;
+    console.log(arr.h);
+  } else if (arr.h === 9) {
+    hair.setAttribute("class", "hairstyle10");
+    arr.h++;
+    console.log(arr.h);
+  } else if (arr.h === 10) {
+    hair.setAttribute("class", "hairstyle11");
+    arr.h++;
+    console.log(arr.h);
+  } else if (arr.h === 11) {
+    hair.setAttribute("class", "hairstyle12");
+    arr.h++;
+    console.log(arr.h);
+  } else if (arr.h === 12) {
+    hair.setAttribute("class", "hairstyle13");
     arr.h = 0;
     console.log(arr.h);
   }
@@ -47,7 +83,7 @@ function nextDress() {
   if (arr.d === 0) {
     dress.setAttribute("class", "dress1");
     arr.d++;
-    console.log(arr.h);
+    console.log(arr.d);
   } else if (arr.d === 1) {
     dress.setAttribute("class", "dress2");
     arr.d++;
@@ -66,6 +102,82 @@ function nextDress() {
     console.log(arr.d);
   } else if (arr.d === 5) {
     dress.setAttribute("class", "dress6");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 6) {
+    dress.setAttribute("class", "dress7");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 7) {
+    dress.setAttribute("class", "dress8");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 8) {
+    dress.setAttribute("class", "dress9");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 9) {
+    dress.setAttribute("class", "dress10");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 10) {
+    dress.setAttribute("class", "dress11");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 11) {
+    dress.setAttribute("class", "dress12");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 12) {
+    dress.setAttribute("class", "dress13");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 13) {
+    dress.setAttribute("class", "dress14");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 14) {
+    dress.setAttribute("class", "dress15");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 15) {
+    dress.setAttribute("class", "dress16");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 16) {
+    dress.setAttribute("class", "dress17");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 17) {
+    dress.setAttribute("class", "dress18");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 18) {
+    dress.setAttribute("class", "dress19");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 19) {
+    dress.setAttribute("class", "dress20");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 20) {
+    dress.setAttribute("class", "dress21");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 21) {
+    dress.setAttribute("class", "dress22");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 22) {
+    dress.setAttribute("class", "dress23");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 23) {
+    dress.setAttribute("class", "dress24");
+    arr.d++;
+    console.log(arr.d);
+  } else if (arr.d === 24) {
+    dress.setAttribute("class", "dress25");
     arr.d = 0;
     console.log(arr.d);
   }
@@ -73,39 +185,151 @@ function nextDress() {
 
 function nextJewel() {
   console.log("inside function nextJewel");
-  var hair = document.getElementById("jewel");
+  var jewellery = document.getElementById("jewel");
   console.log(arr.j);
   if (arr.j === 0) {
-    hair.setAttribute("class", "jewel1");
+    jewellery.setAttribute("class", "jewel1");
     arr.j++;
     console.log(arr.j);
   } else if (arr.j === 1) {
-    hair.setAttribute("class", "jewel2");
+    jewellery.setAttribute("class", "jewel2");
     arr.j++;
     console.log(arr.j);
   } else if (arr.j === 2) {
-    hair.setAttribute("class", "jewel3");
+    jewellery.setAttribute("class", "jewel3");
     arr.j++;
     console.log(arr.j);
   } else if (arr.j === 3) {
-    hair.setAttribute("class", "jewel4");
+    jewellery.setAttribute("class", "jewel4");
     arr.j++;
     console.log(arr.j);
   } else if (arr.j === 4) {
-    hair.setAttribute("class", "jewel5");
+    jewellery.setAttribute("class", "jewel5");
     arr.j++;
     console.log(arr.j);
   } else if (arr.j === 5) {
-    hair.setAttribute("class", "jewel6");
+    jewellery.setAttribute("class", "jewel6");
     arr.j++;
     console.log(arr.j);
   } else if (arr.j === 6) {
-    hair.setAttribute("class", "jewel7");
+    jewellery.setAttribute("class", "jewel7");
     arr.j++;
     console.log(arr.j);
   } else if (arr.j === 7) {
-    hair.setAttribute("class", "jewel8");
+    jewellery.setAttribute("class", "jewel8");
     arr.j = 0;
     console.log(arr.j);
+  }
+}
+
+function nextBag() {
+  console.log("inside function nextBag");
+  var bag = document.getElementById("bag");
+  console.log(arr.bg);
+  if (arr.bg === 0) {
+    bag.setAttribute("class", "bag1");
+    arr.bg++;
+    console.log(arr.bg);
+  } else if (arr.bg === 1) {
+    bag.setAttribute("class", "bag2");
+    arr.bg++;
+    console.log(arr.bg);
+  } else if (arr.bg === 2) {
+    bag.setAttribute("class", "bag3");
+    arr.bg++;
+    console.log(arr.bg);
+  } else if (arr.bg === 3) {
+    bag.setAttribute("class", "bag4");
+    arr.bg++;
+    console.log(arr.bg);
+  } else if (arr.bg === 4) {
+    bag.setAttribute("class", "bag5");
+    arr.bg++;
+    console.log(arr.bg);
+  } else if (arr.bg === 5) {
+    bag.setAttribute("class", "bag6");
+    arr.bg++;
+    console.log(arr.bg);
+  } else if (arr.bg === 6) {
+    bag.setAttribute("class", "bag7");
+    arr.bg++;
+    console.log(arr.bg);
+  } else if (arr.bg === 7) {
+    bag.setAttribute("class", "bag8");
+    arr.bg = 0;
+    console.log(arr.bg);
+  }
+}
+
+function nextScarf() {
+  console.log("inside function nextScarf");
+  var scarf = document.getElementById("scarf");
+  console.log(arr.sc);
+  if (arr.sc === 0) {
+    scarf.setAttribute("class", "scarf0");
+    arr.sc++;
+    console.log(arr.sc);
+  } else if (arr.sc === 1) {
+    scarf.setAttribute("class", "scarf1");
+    arr.sc++;
+    console.log(arr.sc);
+  } else if (arr.sc === 2) {
+    scarf.setAttribute("class", "scarf2");
+    arr.sc++;
+    console.log(arr.sc);
+  } else if (arr.sc === 3) {
+    scarf.setAttribute("class", "scarf3");
+    arr.sc++;
+    console.log(arr.sc);
+  } else if (arr.sc === 4) {
+    scarf.setAttribute("class", "scarf4");
+    arr.sc = 0;
+    console.log(arr.sc);
+  }
+}
+
+function nextSunglass() {
+  console.log("inside nextSunglass");
+  var sunglass = document.getElementById("sunglass");
+  if (arr.sg === 0) {
+    sunglass.setAttribute("class", "sunglass1");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 1) {
+    sunglass.setAttribute("class", "sunglass2");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 2) {
+    sunglass.setAttribute("class", "sunglass3");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 3) {
+    sunglass.setAttribute("class", "sunglass4");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 4) {
+    sunglass.setAttribute("class", "sunglass5");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 5) {
+    sunglass.setAttribute("class", "sunglass6");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 6) {
+    sunglass.setAttribute("class", "sunglass7");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 7) {
+    sunglass.setAttribute("class", "sunglass8");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 8) {
+    sunglass.setAttribute("class", "sunglass9");
+    arr.sg++;
+    console.log(arr.sg);
+  } else if (arr.sg === 9) {
+    sunglass.setAttribute("class", "sunglass10");
+    arr.sg = 0;
+    console.log(arr.sg);
   }
 }
