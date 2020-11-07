987e84dc83686e8f129a5509909c8371dfee385b
Varsha <varshabisht04@gmail.com>
2020-11-07 23:34:48 +0530

    updated

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ function nrmlImg(x) {
   x.style.width = "55px";
 }
 var arr = { h: 0, d: 0, j: 0, bg: 0, sc: 0, sg: 0 };
-console.log(arr.h);
-console.log(arr.d);
-console.log(arr.j);
-console.log(arr.bg);
-console.log(arr.sc);
-console.log(arr.sg);
 
 var d1 = 0,
   h1 = 0;
@@ -97,9 +91,9 @@ function compareFunction5() {
 
 //functionNextHair
 function nextHair() {
-  console.log("inside function nexthair");
+  //console.log("inside function nexthair");
   var hair = document.getElementById("hairstyle");
-  console.log(arr.h);
+  //console.log(arr.h);
   if (arr.h === 0) {
     hair.setAttribute("class", "hairstyle1");
     arr.h++;
@@ -160,56 +154,44 @@ function nextHair() {
 //let dress_click = document.getElementById("dress");
 //dress_click.onclick = nextDress;
 function nextDress() {
-  console.log("inside dress function");
+  //console.log("inside dress function");
   var dress = document.getElementById("dress");
   if (arr.d === 0) {
     dress.setAttribute("class", "dress1");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 1) {
     dress.setAttribute("class", "dress2");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 2) {
     dress.setAttribute("class", "dress3");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 3) {
     dress.setAttribute("class", "dress4");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 4) {
     dress.setAttribute("class", "dress5");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 5) {
     dress.setAttribute("class", "dress6");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 6) {
     dress.setAttribute("class", "dress7");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 7) {
     dress.setAttribute("class", "dress8");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 8) {
     dress.setAttribute("class", "dress9");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 9) {
     dress.setAttribute("class", "dress10");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 10) {
     dress.setAttribute("class", "dress11");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 11) {
     dress.setAttribute("class", "dress12");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 12) {
     dress.setAttribute("class", "dress13");
     arr.d++;
@@ -217,47 +199,36 @@ function nextDress() {
   } else if (arr.d === 13) {
     dress.setAttribute("class", "dress14");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 14) {
     dress.setAttribute("class", "dress15");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 15) {
     dress.setAttribute("class", "dress16");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 16) {
     dress.setAttribute("class", "dress17");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 17) {
     dress.setAttribute("class", "dress18");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 18) {
     dress.setAttribute("class", "dress19");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 19) {
     dress.setAttribute("class", "dress20");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 20) {
     dress.setAttribute("class", "dress21");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 21) {
     dress.setAttribute("class", "dress22");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 22) {
     dress.setAttribute("class", "dress23");
     arr.d++;
-    console.log(arr.d);
   } else if (arr.d === 23) {
     dress.setAttribute("class", "dress24");
     arr.d = 0;
-    console.log(arr.d);
   }
 }
 
@@ -268,35 +239,27 @@ function nextJewel() {
   if (arr.j === 0) {
     jewellery.setAttribute("class", "jewel1");
     arr.j++;
-    console.log(arr.j);
   } else if (arr.j === 1) {
     jewellery.setAttribute("class", "jewel2");
     arr.j++;
-    console.log(arr.j);
   } else if (arr.j === 2) {
     jewellery.setAttribute("class", "jewel3");
     arr.j++;
-    console.log(arr.j);
   } else if (arr.j === 3) {
     jewellery.setAttribute("class", "jewel4");
     arr.j++;
-    console.log(arr.j);
   } else if (arr.j === 4) {
     jewellery.setAttribute("class", "jewel5");
     arr.j++;
-    console.log(arr.j);
   } else if (arr.j === 5) {
     jewellery.setAttribute("class", "jewel6");
     arr.j++;
-    console.log(arr.j);
   } else if (arr.j === 6) {
     jewellery.setAttribute("class", "jewel7");
     arr.j++;
-    console.log(arr.j);
   } else if (arr.j === 7) {
     jewellery.setAttribute("class", "jewel8");
     arr.j = 0;
-    console.log(arr.j);
   }
 }
 
@@ -307,62 +270,47 @@ function nextBag() {
   if (arr.bg === 0) {
     bag.setAttribute("class", "bag1");
     arr.bg++;
-    console.log(arr.bg);
   } else if (arr.bg === 1) {
     bag.setAttribute("class", "bag2");
     arr.bg++;
-    console.log(arr.bg);
   } else if (arr.bg === 2) {
     bag.setAttribute("class", "bag3");
     arr.bg++;
-    console.log(arr.bg);
   } else if (arr.bg === 3) {
     bag.setAttribute("class", "bag4");
     arr.bg++;
-    console.log(arr.bg);
   } else if (arr.bg === 4) {
     bag.setAttribute("class", "bag5");
     arr.bg++;
-    console.log(arr.bg);
   } else if (arr.bg === 5) {
     bag.setAttribute("class", "bag6");
     arr.bg++;
-    console.log(arr.bg);
   } else if (arr.bg === 6) {
     bag.setAttribute("class", "bag7");
     arr.bg++;
-    console.log(arr.bg);
   } else if (arr.bg === 7) {
     bag.setAttribute("class", "bag8");
     arr.bg = 0;
-    console.log(arr.bg);
   }
 }
 
 function nextScarf() {
-  console.log("inside function nextScarf");
   var scarf = document.getElementById("scarf");
-  console.log(arr.sc);
   if (arr.sc === 0) {
     scarf.setAttribute("class", "scarf0");
     arr.sc++;
-    console.log(arr.sc);
   } else if (arr.sc === 1) {
     scarf.setAttribute("class", "scarf1");
     arr.sc++;
-    console.log(arr.sc);
   } else if (arr.sc === 2) {
     scarf.setAttribute("class", "scarf2");
     arr.sc++;
-    console.log(arr.sc);
   } else if (arr.sc === 3) {
     scarf.setAttribute("class", "scarf3");
     arr.sc++;
-    console.log(arr.sc);
   } else if (arr.sc === 4) {
     scarf.setAttribute("class", "scarf4");
     arr.sc = 0;
-    console.log(arr.sc);
   }
 }
 
@@ -372,42 +320,32 @@ function nextSunglass() {
   if (arr.sg === 0) {
     sunglass.setAttribute("class", "sunglass1");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 1) {
     sunglass.setAttribute("class", "sunglass2");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 2) {
     sunglass.setAttribute("class", "sunglass3");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 3) {
     sunglass.setAttribute("class", "sunglass4");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 4) {
     sunglass.setAttribute("class", "sunglass5");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 5) {
     sunglass.setAttribute("class", "sunglass6");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 6) {
     sunglass.setAttribute("class", "sunglass7");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 7) {
     sunglass.setAttribute("class", "sunglass8");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 8) {
     sunglass.setAttribute("class", "sunglass9");
     arr.sg++;
-    console.log(arr.sg);
   } else if (arr.sg === 9) {
     sunglass.setAttribute("class", "sunglass10");
     arr.sg = 0;
-    console.log(arr.sg);
   }
 }
